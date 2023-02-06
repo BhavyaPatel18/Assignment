b1a4189aa98f009fc1a270739063acfd1810330e
BhavyaP07 <pate4964@sheridancollege.ca>
2023-02-05 22:11:50 -0500

    New message

---
+++ Barbarine.py
@@ -0,0 +1,9 @@
+"""Module for data and logic associated with the Barbarine role"""
+
+def Barbarine():
+    strength = 2
+    dexterity = 1
+    intelligence = 0
+    health = 2
+    print("Barbarine:\nStrenght-",strength,"\nDexterity-",dexterity,"\nIntelligence-",intelligence,"\nHealth-",health)
+    return True
---
+++ Wizard.py
@@ -0,0 +1,9 @@
+"""Module for data and logic associated with the Barbarine role"""
+
+def Wizard():
+    strength = 0
+    dexterity = 2
+    intelligence = 2
+    health = 1
+    print("Wizard:\nStrenght-",strength,"\nDexterity-",dexterity,"\nIntelligence-",intelligence,"\nHealth-",health)
+    return True
